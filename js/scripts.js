c5cfb2125d8dca569f3119c790f8c19096f12790
nhu finney <student@epicodus.com>
2015-04-09 19:26:03 -0700

    finish html file.

--- js/scripts.js
+++ js/scripts.js
@@ -4,36 +4,66 @@ var wordCount = function(input){
     var count = 1;
     var word_object = {};
 
-    if (input_array.length === 1){
-        var key = input_array[0];
-        word_object[key] = count;
+    // if (input_array.length === 1){
+    //     var key = input_array[0];
+    //     word_object[key] = count;
+    //
+    // };
+    //
+    // if (input_array.length >1){
+    //
+    //     input_array.forEach(function(one){
+    //         var keys_array = Object.keys(word_object);
+    //         if (keys_array.length == 0) {
+    //             word_object[one] = count;
+    //         } else if(keys_array.indexOf(one) !== -1){
+    //             var one_count = word_object[one];
+    //             one_count = one_count + 1;
+    //             word_object[one] = one_count;
+    //         } else {
+    //             word_object[one] = count;
+    //         }
+    //  });
+    //  };
 
-    };
+     for (var i in input_array){
+       if (!word_object.hasOwnProperty(input_array[i])){
+         word_object[input_array[i]] = 1;
+       } else {
+         word_object[input_array[i]]++;
+       }
+     }
 
-    if (input_array.length >1){
+     var sorted_words = [];
+     for (var word in word_object){
+       sorted_words.push([word, word_object[word]]);
+     }
 
-        input_array.forEach(function(one){
-            var keys_array = Object.keys(word_object);
-            if (keys_array.length == 0) {
-                word_object[one] = count;
-            } else if(keys_array.indexOf(one) !== -1){
-                var one_count = word_object[one];
-                one_count = one_count + 1;
-                word_object[one] = one_count;
-            } else {
-                word_object[one] = count;
-            }
+
+     sorted_words.sort(function(word1, word2){
+       return word2[1] - word1[1];
      });
-     };
 
+     var final = [];
+     for (var i in sorted_words){
+       final.push(sorted_words[i][0]);
+     }
 
-
-
-
-
-
-
-
-    return word_object;
+    return sorted_words;
 
 };
+
+jQuery(document).ready(function(event){
+  $("#word-order").submit(function(event) {
+  var words = $("#words").val();
+  var order = wordCount(words);
+  $("#order").text(order[0]);
+
+  for (var i in order){
+    $("#order").append(", " + order[i]);
+  }
+
+  $("#result").show();
+  event.preventDefault();
+});
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,27 @@
 describe('wordCount', function(){
 
-    it("it counts one word, no duplicate", function(){
-        expect(wordCount("aa")).to.eql({aa: 1});
-    });
+    // it("it counts one word, no duplicate", function(){
+    //     expect(wordCount("aa")).to.eql({aa: 1});
+    // });
+    //
+    // it("it counts two words, no duplicate", function(){
+    //     expect(wordCount("hello world")).to.eql({hello: 1, world: 1});
+    // });
+    //
+    // it("it count four words, no duplicate", function(){
+    //     expect(wordCount("hello world my love")).to.eql({hello: 1, world: 1, my:1, love: 1});
+    // });
+    //
+    // it("it count five words, some duplicates", function(){
+    //     expect(wordCount("world world my world my")).to.eql({world: 3, my: 2});
+    // });
 
-    it("it counts two words, no duplicate", function(){
-        expect(wordCount("hello world")).to.eql({hello: 1, world: 1});
-    });
-
-    it("it count four words, no duplicate", function(){
-        expect(wordCount("hello world my love")).to.eql({hello: 1, world: 1, my:1, love: 1});
+    it("it count five words, some duplicates", function(){
+        expect(wordCount("oh oh oh world on world my oh world my")).to.eql({oh :4, world: 3, on:1, my:2});
     });
 
     it("it count five words, some duplicates", function(){
-        expect(wordCount("world world my world my")).to.eql({world: 3, my: 2});
+        expect(wordCount("with to this to to my to with")).to.eql({with: 2, to:4, this: 1, my:1});
     });
 
 
